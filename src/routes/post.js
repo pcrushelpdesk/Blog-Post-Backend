be2b9c13287bb4e4d38caa34e77898c2398a524b
edmondchoi <superstar.ed@gmail.com>
2024-03-14 19:19:18 -0700

    working backend

---
+++ src/routes/post.js
@@ -0,0 +1,21 @@
+//Import the Express
+const express = require('express');
+const router = express.Router();
+const {
+    createPost,
+    getAllPosts,
+    getPost,
+    updatePost, 
+    deletePost
+} = require('../controllers/post');
+
+const { authMiddleware } = require("../middleware/authMiddleware")
+
+//Create Routers
+router.post('/', authMiddleware, createPost);
+router.get('/', getAllPosts);
+router.get('/:id', getPost);
+router.patch('/:id', authMiddleware, updatePost);
+router.delete('/:id', authMiddleware, deletePost);
+
+module.exports = router;
